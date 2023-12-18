a222b6fe3f0e48aa83b858c4ac6eadbefa6f7aff
nesrine_guesmi <70206287+NesrineGuesmii@users.noreply.github.com>
2023-12-18 13:55:33 +0100

    adding tests

--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -6,4 +6,29 @@ require('chai')
 
 contract('Memory Token', (accounts) => {
   // code goes here...
+  let token
+
+  before(async () => {
+    token = await MemoryToken.deployed()
+  })
+  
+  describe('deploymenet', async() => {
+    it('deploys successfully', async() => {
+      const address = token.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+    })
+
+    it('has a name', async () => {
+      const name = await token.name()
+      assert.equal(name, 'Memory Token')
+    })
+
+    it('has a symbol', async () => {
+      const symbol = await token.symbol()
+      assert.equal(symbol, 'MEMORY')
+    })
+  })
 })
